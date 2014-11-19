96742f7af5d0a89d07ef69d5e2c13cf6b1a93fd2
Sergio <soyicu@gmail.com>
2014-11-19 22:20:39 +0100

    Ipad StoryBoard

---
+++ AppReserva/PrincipalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PrincipalViewController.swift
+//  AppReserva
+//
+//  Created by Icu on 19/11/14.
+//  Copyright (c) 2014 IcuScc. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
