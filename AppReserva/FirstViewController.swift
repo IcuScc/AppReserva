26452e323fbbda86fba338e2d0e5571c47b6931e
Sergio <soyicu@gmail.com>
2014-11-19 21:10:28 +0100

    Inicio
    Inicio

---
+++ AppReserva/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AppReserva
+//
+//  Created by Icu on 19/11/14.
+//  Copyright (c) 2014 IcuScc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
